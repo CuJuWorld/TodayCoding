18fa4785d696af0b8a1920a60e3e6f8e1339afd2
Sports-Ai <45550571+CuJuWorld@users.noreply.github.com>
2025-03-12 17:35:18 +0800

    committed

--- ex2-regular-function.js
+++ ex2-regular-function.js
@@ -1,4 +1,4 @@
-Copy;
+// Copy;
 // 1. 將箭頭函數改寫為常規函數
 function add(a, b) {
     return a + b;
@@ -23,6 +23,28 @@ function sumArray(numbers) {
     }
     return total;
 }
-console.log(sumArray([1, 2, 3, 4])); // 輸出: 10
+
+function AsumArray(numbers) {
+    // 檢查輸入是否為數組
+    if (!Array.isArray(numbers)) {
+        throw new Error("INVALID input");
+    }
+
+    let total = 0;
+
+    // 使用 for 循環計算總和
+    for (let i = 0; i < numbers.length; i++) {
+        if (typeof numbers[i] !== "number") {
+            throw new Error("INVALID input");
+        }
+        total += numbers[i];
+    }
+    return total;
+}
+console.log(sumArray('\n'+ [1, 2, 3, 4])); // 輸出: 10
 console.log(sumArray([1.5, 2.5, 3])); // 輸出: 7
-console.log(sumArray("not an array")); // 輸出: 錯誤：輸入無效
+console.log(sumArray("FIRST ARRAY")); // 輸出: 錯誤：輸入無效
+
+console.log(AsumArray([2, 3, 4, 5])); // 輸出: 14
+console.log(AsumArray([2.5, 3.5, 4, 7])); // 輸出: 17
+console.log(AsumArray("SECOND ARRAY")); // INVALID Input
